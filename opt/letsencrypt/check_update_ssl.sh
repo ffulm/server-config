79d2f527ebe441589bf1b3d08a956233f2aaa5e3
Martin Kerler <bugs.m1@kkk-web.de>
2016-04-14 17:26:15 +0200

    add TODO marker

--- opt/letsencrypt/check_update_ssl.sh
+++ opt/letsencrypt/check_update_ssl.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 service lighttpd stop
+# TODO: use renew option in 0.4.0 letsencrypt clients here?
 if ! /opt/letsencrypt/letsencrypt-auto certonly -tvv --email info@freifunk-ulm.de --agree-tos --standalone --keep -d $(hostname) > /var/log/letsencrypt/renew.log 2>&1 ; then
     echo Automated renewal failed:
     cat /var/log/letsencrypt/renew.log
