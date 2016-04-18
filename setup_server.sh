652d72667849a9534fc01c7b3f12ec30f7de026f
Martin Kerler <bugs.m1@kkk-web.de>
2016-04-18 19:38:11 +0200

    change server setup to meshviewer - second part

--- setup_server.sh
+++ setup_server.sh
@@ -153,19 +153,36 @@ if [ "$setup_webserver" = "true" ]; then
 		mkdir -p /var/www/
 		cp -r var/www/* /var/www/
 
-		echo "(I) Add ffmap-d3"
-		apt-get install --assume-yes make git
-		git clone https://github.com/freifunk-bielefeld/ffmap-d3.git
-		cd ffmap-d3
-		sed -i "s/ffbi-/ffulm-/g" config.js
-		sed -i "s/gotham.freifunk.net/www.freifunk-$community_id.de/g" config.js
-		sed -i "s/gotham/$community_id/g" config.js
-		sed -i "s/Gotham/$community_name/g" config.js
-		sed -i "s/fdef:17a0:ffb1:300::/$ff_prefix/g" config.js
-		make
-		cp -r www/* /var/www/
-		cd ..
-		rm -rf ffmap-d3
+		#echo "(I) Add ffmap-d3"
+		#apt-get install --assume-yes make git
+		#git clone https://github.com/freifunk-bielefeld/ffmap-d3.git
+		#cd ffmap-d3
+		#sed -i "s/ffbi-/ffulm-/g" config.js
+		#sed -i "s/gotham.freifunk.net/www.freifunk-$community_id.de/g" config.js
+		#sed -i "s/gotham/$community_id/g" config.js
+		#sed -i "s/Gotham/$community_name/g" config.js
+		#sed -i "s/fdef:17a0:ffb1:300::/$ff_prefix/g" config.js
+		#make
+		#cp -r www/* /var/www/
+		#cd ..
+		#rm -rf ffmap-d3
+
+                echo "(I) Add meshviewer"
+                mkdir -p /var/www/meshviewer/
+                apt-get install --assume-yes git npm nodejs-legacy ruby-sass
+                git clone https://github.com/freifunk-bielefeld/meshviewer.git
+                cd meshviewer
+                npm install
+                npm install grunt-cli
+                node_modules/.bin/grunt
+                # copy config to build
+                cp ../etc/meshviewer/config.json build/
+                # copy build to webroot
+		cp -r build/* /var/www/meshviewer/
+                cd ..
+                # destroy build
+                rm -rf meshviewer
+
 
 		chown -R www-data:www-data /var/www
 	}
